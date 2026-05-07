4c02c1ab642179cf26597e7502916811c46498e6
Berkay TİKENOĞLU <berkaytikenoglu@gmail.com>
2026-05-07 12:50:53 +0300

    feat: implement ChatList component with filtering, search functionality, and infinite scrolling support

--- src/components/modules/chat/widgets/ChatList.tsx
+++ src/components/modules/chat/widgets/ChatList.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { useChat } from '../../../../context/ChatContext';
 import { useSocket } from '../../../../context/SocketContext';
